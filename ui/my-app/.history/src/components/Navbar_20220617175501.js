589ee8663ac00ae74e21bf65cda95759885a4cce
AndinBudakova <ab51129@ubt-uni.net>
2022-06-17 17:55:31 +0200

    push

---
+++ ui/my-app/.history/src/components/Navbar_20220617175501.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
+import "../styles/navbar.css";
+
+function Navbar() {
+  return (
+    <div>
+      <div class="navbarApp">
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+          <li className="nav-item- m-1">
+              <nav className="btn btn-light btn-outline-primary">
+                <Link to="/">Log out</Link>
+              </nav>
+            </li>
+            <li className="nav-item- m-1">
+              <nav className="btn btn-light btn-outline-primary">
+                <Link to="/lenda">Lenda</Link>
+              </nav>
+            </li>
+            <li className="nav-item- m-1">
+              <nav className="btn btn-light btn-outline-primary">
+                <Link to="/profesori">
+                <span class="nameLink">Profesori</span>
+                </Link>
+              </nav>
+            </li>
+            <li className="nav-item- m-1">
+            <nav className="btn btn-light btn-outline-primary">
+              <Link to="/libri">
+              <span class="nameLink">Libri</span>
+              </Link>
+            </nav>
+          </li>
+          <li className="nav-item- m-1">
+            <nav className="btn btn-light btn-outline-primary">
+              <Link to="/orari">
+              <span class="nameLink">Orari</span>
+              </Link>
+            </nav>
+          </li>
+          <li className="nav-item- m-1">
+            <nav className="btn btn-light btn-outline-primary">
+              <Link to="/paralelja">
+              <span class="nameLink">Paralelja</span>
+              </Link>
+            </nav>
+          </li>
+          <li className="nav-item- m-1">
+            <nav className="btn btn-light btn-outline-primary">
+              <Link to="/klasa">
+              <span class="nameLink">Klasa</span>
+              </Link>
+            </nav>
+          </li>
+          <li className="nav-item- m-1">
+            <nav className="btn btn-light btn-outline-primary">
+              <Link to="/nxenesi">
+              <span class="nameLink">Nxenesi</span>
+              </Link>
+            </nav>
+          </li>
+          <li className="nav-item- m-1">
+            <nav className="btn btn-light btn-outline-primary">
+              <Link to="/nota">
+              <span class="nameLink">Nota</span>
+              </Link>
+            </nav>
+          </li>
+          <li className="nav-item- m-1">
+            <nav className="btn btn-light btn-outline-primary">
+              <Link to="/vleresimi">
+              <span class="nameLink">Vleresimi</span>
+              </Link>
+            </nav>
+          </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

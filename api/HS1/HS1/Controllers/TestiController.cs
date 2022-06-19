96060df010a119673bfce4652e0be60d8637f3ba
AndinBudakova <ab51129@ubt-uni.net>
2022-06-19 23:55:26 +0200

    Krijimi i backend crud Testi

---
+++ api/HS1/HS1/Controllers/TestiController.cs
@@ -0,0 +1,133 @@
+﻿using HS1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace HS1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestiController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public TestiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select TestiId,Profesori,Profesori,Ora,Data
+                            from
+                            dbo.Testi
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Testi te)
+        {
+            string query = @"
+                            insert into dbo.Testi
+                            ()
+                            values (@Profesori,@Lenda,@Ora,@Data)
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Profesori", te.Profesori);
+                    myCommand.Parameters.AddWithValue("@Lenda", te.Lenda);
+                    myCommand.Parameters.AddWithValue("@Ora", te.Ora);
+                    myCommand.Parameters.AddWithValue("@Data", te.Data);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+        [HttpPut]
+        public JsonResult Put(Testi te)
+        {
+            string query = @"
+                            update dbo.Testi
+                            set Profesori= @Profesori,
+                            Lenda= @Lenda
+                                Ora= @Ora
+                             Data= @Data
+                            where TestiId=@TestiId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@TestiId", te.TestiId);
+                    myCommand.Parameters.AddWithValue("@Profesori", te.Profesori);
+                    myCommand.Parameters.AddWithValue("@Lenda", te.Lenda);
+                    myCommand.Parameters.AddWithValue("@Ora", te.Ora);
+                    myCommand.Parameters.AddWithValue("@Data", te.Data);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Testi
+                            where TestiId=@TestiId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@TestiId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ api/HS1/HS1/Models/Testi.cs
@@ -0,0 +1,11 @@
+﻿namespace HS1.Models
+{
+    public class Testi
+    {
+        public int TestiId { get; set; }
+        public string Profesori { get; set; }
+        public string Lenda { get; set; }
+        public string Ora { get; set; }
+        public string Data { get; set; }
+    }
+}

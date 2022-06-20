3571139c4be131d50e7447b74dcd91e97eac2fd3
AndinBudakova <ab51129@ubt-uni.net>
2022-06-20 21:49:22 +0200

    Krijimi i Crud KontaktPrind
    Backend, frontend-react

---
+++ api/HS1/HS1/Controllers/KontaktPrindController.cs
@@ -0,0 +1,129 @@
+﻿using HS1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace HS1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KontaktPrindController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public KontaktPrindController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select KontaktPId,Prindi,NrTel
+                            from
+                            dbo.KontaktPrind
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(KontaktPrind kp)
+        {
+            string query = @"
+                            insert into dbo.KontaktPrind
+                            (Prindi, NrTel)
+                            values (@Prindi, @NrTel)
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Prindi", kp.Prindi);
+                    myCommand.Parameters.AddWithValue("@NrTel", kp.NrTel);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+        [HttpPut]
+        public JsonResult Put(KontaktPrind kp)
+        {
+            string query = @"
+                            update dbo.KontaktPrind
+                            set Prindi= @Prindi,
+                            NrTel=@NrTel
+                            where KontaktPId=@KontaktPId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@KontaktPId", kp.KontaktPId);
+                    myCommand.Parameters.AddWithValue("@Prindi", kp.Prindi);
+                    myCommand.Parameters.AddWithValue("@NrTel", kp.NrTel);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.KontaktPrind
+                            where KontaktPId=@KontaktPId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@KontaktPId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ api/HS1/HS1/Models/KontaktPrind.cs
@@ -0,0 +1,9 @@
+﻿namespace HS1.Models
+{
+    public class KontaktPrind
+    {
+        public int KontaktPId { get; set; }
+        public string Prindi { get; set; }
+        public string NrTel { get; set; }
+    }
+}
